7d3330a56a9dece60401626cff9e2aef07f4ae0e
Jake Crespo <jake.crespo@modop.com>
2022-01-05 10:23:36 -0500

    remove files

--- tour-panama-with-us-backup/packages/themes/src/RealEstate/containers/Gallery/styled.components.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import Img from '@pagerland/common/src/components/Img';
-
-export const StyledImg = styled(Img)`
-  cursor: pointer;
-  width: 100%;
-  height: 190px;
-  object-fit: cover;
-`;
